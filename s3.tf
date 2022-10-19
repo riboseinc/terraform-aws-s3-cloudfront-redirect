90ea1d2ad93d083ed58ae9d7e869b61e130f0a69
phuong <phuonghqh@gmail.com>
2022-10-19 23:10:22 +0700

    Upgrade to terraform v1.3.x

--- s3.tf
+++ s3.tf
@@ -21,12 +21,6 @@ resource "aws_s3_bucket_website_configuration" "main" {
 resource "aws_s3_bucket" "main" {
   provider = aws.main
   bucket   = var.fqdn
-  #  acl      = "private"
-  #  policy   = data.aws_iam_policy_document.bucket_policy.json
-
-  #  website {
-  #    redirect_all_requests_to = var.redirect_target
-  #  }
 
   force_destroy = var.force_destroy
 
